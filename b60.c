12ebd65003d92d8ce8ce569bd070c2302ac69f3c
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2022-10-06 22:31:01 +0700

    Create b60.c

---
+++ b60.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+	int arr[100][100];
+	int n, m;
+	int i, j;
+
+	scanf("%d%d", &n, &m);
+	for (i = 0; i < n; i++) {
+		for ( j = 0; j < m; j++) {
+			scanf("%d", &arr[i][j]);
+		}
+	}
+
+	int sum = 0;
+	for (i = 0; i < n; i++) {
+		for ( j = 0; j < m; j++) {
+			sum += arr[i][j];
+		}
+	}
+
+	printf("%d", sum);
+
+	return 0;
+}
